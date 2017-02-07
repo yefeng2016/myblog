fc657c5fae229b2be5c89ef6913ba5b8f6426f09
yefeng2016 <jrtzxh020@foxmail.com>
2017-02-08 00:15:13 +0800

    完成注册功能

--- index.js
+++ index.js
@@ -27,9 +27,16 @@ app.use(session({
     url: config.mongodb// mongodb 地址
   })
 }));
+
+
 // flash 中间件，用来显示通知
 app.use(flash());
 
+// 处理表单及文件上传的中间件
+app.use(require('express-formidable')({
+  uploadDir: path.join(__dirname, 'public/img'),// 上传文件目录
+  keepExtensions: true// 保留后缀
+}));
 // 设置模板全局常量
 app.locals.blog = {
   title: pkg.name,
