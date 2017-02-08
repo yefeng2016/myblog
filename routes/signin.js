5db9d05fab4990b69249656c28c1bf5587497601
叶枫 <jrtzxh020@foxmail.com>
2017-02-08 17:44:49 +0800

    登录1.0

--- routes/signin.js
+++ routes/signin.js
@@ -5,12 +5,14 @@ var checkNotLogin = require('../middlewares/check').checkNotLogin;
 
 // GET /signin 登录页
 router.get('/', checkNotLogin, function(req, res, next) {
-  res.send(req.flash());
+  res.render('signin');
 });
 
 // POST /signin 用户登录
 router.post('/', checkNotLogin, function(req, res, next) {
-  res.send(req.flash());
+  var resq = req;
+  res.writeHead(200);
+   res.json({'name':1});
 });
 
 module.exports = router;
--- routes/signout.js
+++ routes/signout.js
@@ -5,7 +5,9 @@ var checkLogin = require('../middlewares/check').checkLogin;
 
 // GET /signout 登出
 router.get('/', checkLogin, function(req, res, next) {
-  res.send(req.flash());
+  res.session.user = null;
+  res.send(req.flash('success','登出成功'));
+  res.redirect('/posts');
 });
 
 module.exports = router;
